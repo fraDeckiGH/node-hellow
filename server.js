986ca18713e5a6d03ec35636259e1991b58c220e
Francesco De Chirico <fradc16@gmail.com>
2020-08-21 22:15:23 +0200

    little test

---
+++ server.js
@@ -0,0 +1,17 @@
+import { createServer } from "http";
+import { app } from "./app.mjs";
+
+const port = process.env.PORT || 3000;
+
+const server = createServer(app);
+server.listen(port);
+
+
+// >> in node 12.18.3
+// run with (app[.js]):
+// node server.js --experimental-modules
+
+
+// >> in node 14.8.0
+// run with (app.mjs):
+// node server.js
