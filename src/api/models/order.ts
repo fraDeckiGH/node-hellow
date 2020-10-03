d492789665d01689b6f92bec27943efb1d381a4d
fdc Windows <fradc16@gmail.com>
2020-10-03 22:06:13 +0200

    master: 09 done

--- src/api/models/order.ts
+++ src/api/models/order.ts
@@ -1,30 +1,37 @@
 import { model, Schema, Types } from "mongoose";
+import mongooseAutopopulate from "mongoose-autopopulate";
+import { sortSchemaKeys } from "../../util";
 
 
 const schema = new Schema({
-	quantity: {
-		default: 1,
-		type: Number,
-	},
-	
 	product: {
+		autopopulate: true,
 		ref: 'Product',
 		required: true,
 		type: Types.ObjectId,
 	},
+	
+	quantity: {
+		default: 1,
+		type: Number,
+	},
 })
+	.plugin(mongooseAutopopulate)
+	
 	// https://mongoosejs.com/docs/api/document.html#document_Document-toJSON
 
 	// options to apply when this schema is applied to JSON
 	// e.g API response
-	.set('toJSON', { 
+	.set('toJSON', {
+		transform: (undefined, ret) => sortSchemaKeys(ret),
 		// useProjection: true,
 		versionKey: false
 	})
 
 	// options to apply when this schema is applied to Object
 	// e.g console.log
-	.set('toObject', { 
+	.set('toObject', {
+		transform: (undefined, ret) => sortSchemaKeys(ret),
 		// useProjection: true,
 		versionKey: false
 	});
--- src/util.ts
+++ src/util.ts
@@ -3,6 +3,8 @@
 import { Response } from "express";
 
 
+
+
 export function apiError(e: any, res: Response) {
 	console.error(e);
 	
@@ -12,7 +14,24 @@ export function apiError(e: any, res: Response) {
 }
 
 
-
+export function sortSchemaKeys(ret: any) {
+	// https://mongoosejs.com/docs/api/document.html#document_Document-toJSON
+	// (scroll a bit to the bottom)
+	
+	// console.log("doc", doc); // gives error
+	// console.log("ret", ret);
+	// console.log("opts", opts);
+	
+	let newObj: any = {};
+	
+	Object.keys(ret)
+		.sort()
+		.forEach((key: string) => {
+			newObj[key] = ret[key];
+		});
+	
+	return newObj;
+}
 
 
 
